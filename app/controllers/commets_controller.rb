40a55196a58490d21acf76113d4ce5fd79615950
XiawPohr <paradise121329@gmail.com>
2017-03-24 18:34:42 +0800

    commets#destroy complete

--- app/controllers/commets_controller.rb
+++ app/controllers/commets_controller.rb
@@ -1,9 +1,19 @@
 class CommetsController < ApplicationController
+	before_action :authenticate_user!, only: [:create, :destroy]
+
 	def create
 		disaster = Disaster.find(params[:disaster_id])
 		comment = Commet.new(comment_params)
 		comment.disaster = disaster
+		comment.user = current_user
 		comment.save
+		redirect_to disaster_path(disaster)
+	end
+
+	def destroy
+		disaster = Disaster.find(params[:disaster_id])
+		comment = Commet.find(params[:id])
+		comment.destroy
 		redirect_to disaster_path(disaster)
 	end
 
