d343a197702e1d13f63883f033bed546a33124d1
XiawPohr <paradise121329@gmail.com>
2017-03-24 16:13:49 +0800

    disaster#create complete

--- app/controllers/disasters_controller.rb
+++ app/controllers/disasters_controller.rb
@@ -1,5 +1,18 @@
 class DisastersController < ApplicationController
 	def index
 		@disasters = Disaster.all
+		@disaster = Disaster.new
 	end
+
+	def create
+		disaster = Disaster.new(disaster_params)
+		disaster.save
+		redirect_to disasters_path
+	end
+
+	private 
+
+		def disaster_params
+			params.require(:disaster).permit(:title, :content, :category_id)
+		end
 end
